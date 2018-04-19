2efd8bf174f30b3933d1c06dbd216e4683efd3c0
thristram <thristram>
2018-04-19 15:29:27 +0800

    Initial Commit

---
+++ the 24 GameTests/the_24_GameTests.swift
@@ -0,0 +1,36 @@
+//
+//  the_24_GameTests.swift
+//  the 24 GameTests
+//
+//  Created by Fangchen Li on 4/19/18.
+//  Copyright © 2018 Fangchen Li. All rights reserved.
+//
+
+import XCTest
+@testable import the_24_Game
+
+class the_24_GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
